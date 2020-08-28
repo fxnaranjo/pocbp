24fda50886d204ed8771802c310d08aa8576aa23
Francisco Naranjo <fxnaranjo@gmail.com>
2020-08-27 21:49:27 -0500

    WAS

---
+++ deleteAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "********** DELETING WAS **********"
+oc delete -f /home/test/BancoPichincha/WAS/was.yaml
+
+
+
+echo "**********************************"
+echo "DONE"
---
+++ deployAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "********** DEPLOYING WAS **********"
+oc apply -f /home/test/BancoPichincha/WAS/was.yaml
+
+
+
+echo "**********************************"
+echo "DONE"
