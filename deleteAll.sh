549737a0612d2b94bf904da03b3f6afff59995a0
Francisco Naranjo <fxnaranjo@gmail.com>
2020-08-27 23:56:44 -0500

    IIB-WAS

--- deleteAll.sh
+++ deleteAll.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+echo "***********************************************************************"
+echo "Hello,what do you want to deploy today:"
+echo "1.- NGINX"
+echo "2.- Websphere Application Server"
+echo "3.- IBM Integration Bus"
+echo "***********************************************************************"
 
-echo "********** DELETING WAS **********"
-oc delete -f /home/test/BancoPichincha/WAS/was.yaml
 
+read varname
 
-
-echo "**********************************"
-echo "DONE"
+case "$varname" in
+        1)
+            echo "********** DELETING NGINX **********"
+            oc delete -f NGINX/nginx.yaml
+            echo "********** DONE NGINX**********"
+            ;;
+        2)
+            echo "********** DELETING WAS **********"
+            oc delete -f WAS/was.yaml
+            echo "********** DONE WAS**********"
+            ;;
+        3)
+            echo "********** DELETING IIB **********"
+            oc delete -f IIB/iib.yaml
+            echo "********** DONE IIB**********"
+            ;;
+         
+        *)
+            echo "Not a valid option"
+            exit 1
+ 
+esac
--- deployAll.sh
+++ deployAll.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+echo "***********************************************************************"
+echo "Hello,what do you want to deploy today:"
+echo "1.- NGINX"
+echo "2.- Websphere Application Server"
+echo "3.- IBM Integration Bus"
+echo "***********************************************************************"
 
-echo "********** DEPLOYING WAS **********"
-oc apply -f /home/test/BancoPichincha/WAS/was.yaml
 
+read varname
 
-
-echo "**********************************"
-echo "DONE"
+case "$varname" in
+        1)
+            echo "********** DEPLOYING NGINX **********"
+            oc apply -f NGINX/nginx.yaml
+            echo "********** DONE NGINX**********"
+            ;;
+        2)
+            echo "********** DEPLOYING WAS **********"
+            oc apply -f WAS/was.yaml
+            echo "********** DONE WAS**********"
+            ;;
+        3)
+            echo "********** DEPLOYING IIB **********"
+            oc apply -f IIB/iib.yaml
+            echo "********** DONE IIB**********"
+            ;;
+         
+        *)
+            echo "Not a valid option"
+            exit 1
+ 
+esac
